0d8652baae7082facfce03ee092d4350968b67c0
Filip Novak <fifix.tp@gmail.com>
2025-05-06 16:57:51 +0200

    docs: Added comments to io files

--- src/io/JsonLoader.h
+++ src/io/JsonLoader.h
@@ -1,3 +1,10 @@
+/**
+ * @file JsonLoader.h
+ * @brief Header file for the JsonLoader class for creating a FSM from a Json representation
+ * @author xnovakf00
+ * @date 05.05.2025
+ */
+
 #pragma once
 
 #include "FSM.h"
