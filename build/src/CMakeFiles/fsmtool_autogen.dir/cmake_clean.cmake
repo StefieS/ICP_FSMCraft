dae90145dd471f238bd82f396ebb22f21eac103a
StefieS <mikilesiga@gmail.com>
2025-05-08 00:49:32 +0200

    chore: removed build

--- build/src/CMakeFiles/fsmtool_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/fsmtool_autogen"
-  "fsmtool_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/fsmtool_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
