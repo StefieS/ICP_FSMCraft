5dcb13bf8c57b1c729f828bba68e81a78c1b9977
Filip Novak <fifix.tp@gmail.com>
2025-05-08 18:03:24 +0200

    feat: Initial qtfsm builder from json

---
+++ src/qtfsm/QTfsmBuilder.cpp
@@ -0,0 +1,51 @@
+/**
+*  @file QTfsmBuilder.cpp
+ * @brief Implementation file for a class to build QTFsm from loaded jsonDoc
+ * @author xnovakf00
+ * @date 08.05.2025
+ */
+
+#include "QTfsm.h"
+#include "QTfsmBuilder.h"
+#include "../fsm/State.h"
+#include "../fsm/Transition.h"
+QTfsm& QTfsmBuilder::buildQTfsm(const QJsonDocument& jsonDoc) {
+    JsonLoader loader = JsonLoader();
+    this->innerFsm = loader.fromJson(jsonDoc);
+
+    QTfsm qtfsm = QTfsm(nullptr, this->innerFsm->getName());
+
+    auto states = this->innerFsm->getStates();
+    for (auto state : states) {
+        QString stateName = QString::fromStdString(state.first);
+        QString stateAction = QString::fromStdString(state.second->getActionCode());
+        QAbstractState* toBeSet;
+        if (state.second->isFinalState()) {
+            toBeSet = qtfsm.addFinalState(stateName);
+        } else if (state.second->isInitialState()) {
+            toBeSet = qtfsm.addState(stateName);
+            qtfsm.setInitialState(toBeSet);
+        } else {
+            toBeSet = qtfsm.addState(stateName);
+        }
+        qtfsm.addStateJsAction(toBeSet, stateAction);
+    }
+
+    auto transitions = this->innerFsm->getTransitions();
+    for (auto transition : transitions) {
+        QString srcName = QString::fromStdString(transition->getSource());
+        QString trgtName = QString::fromStdString(transition->getTarget());
+        QString cond = QString::fromStdString(transition->getGuardCondition()); 
+        QState* srcState = qobject_cast<QState*>(qtfsm.getStateByName(srcName));
+        QAbstractState* trgtState = qtfsm.getStateByName(trgtName);
+        qtfsm.addJsTransition(srcState, trgtState, cond);
+    }
+
+    auto variables = this->innerFsm->getInternalVars();
+    for (auto var : variables) {
+        QJSValue val = qtfsm.getJsEngine()->toScriptValue(var.getInitialValue());
+        QString name = QString::fromStdString(var.getName());
+        qtfsm.setJsVariable(name, val);
+    }
+
+}
---
+++ src/qtfsm/QTfsmBuilder.h
@@ -0,0 +1,27 @@
+/**
+*  @file QTfsmBuilder.h
+ * @brief Header file for a class to build QTFsm from loaded jsonDoc
+ * @author xnovakf00
+ * @date 08.05.2025
+ */
+
+#pragma once;
+#include "../fsm/FSM.h"
+#include "../fsm/State.h"
+#include "../fsm/Transition.h"
+#include "../io/JsonLoader.h"
+#include "QTfsm.h"
+class QTfsmBuilder {
+
+private:
+    FSM* innerFsm;
+
+public:
+    /**
+     * @brief Builds QTfsm from json representation of the FSM.
+     * @param jsonDoc json representation
+     * @returns Built QTfsm
+     */
+    QTfsm& buildQTfsm(const QJsonDocument& jsonDoc);
+
+};
