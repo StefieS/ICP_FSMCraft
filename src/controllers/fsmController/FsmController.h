456d3f277135b7b3c9e7ed93bf8de55dcb99820c
Filip Novak <fifix.tp@gmail.com>
2025-05-09 21:08:03 +0200

    chore: Adding Fsm controller header

---
+++ src/controllers/fsmController/FsmController.h
@@ -0,0 +1,14 @@
+/**
+ * @file FsmController.h
+ * @brief Header file for the FsmController class
+ * @author xnovakf00
+ * @author xlesigm00
+ * @date 09.05.2025
+ */
+
+
+#pragma once
+class FsmController {
+private:
+public:
+};
