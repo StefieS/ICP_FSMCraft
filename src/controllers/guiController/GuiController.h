c5f0138f717fc11b607e1d78f459a0d9b18241a5
Filip Novak <fifix.tp@gmail.com>
2025-05-10 22:36:41 +0200

    docs: Added comments

--- src/controllers/guiController/GuiController.h
+++ src/controllers/guiController/GuiController.h
@@ -5,15 +5,33 @@
  * @date 07.05.2025
  */
 
-
 #pragma once
 #include "IMainWindow.h"
 #include "../messages/Message.h"
+
+/**
+ * @class GuiController
+ * @brief Class for controlling the GUI
+ */
 class GuiController {
 private:
+    /**
+     * GUI window to control
+     */
     IMainWindow* gui;
 
 public:
+    /**
+     * @brief Constructor for the controller
+     * @param gui Window to control
+     */
     GuiController(IMainWindow* gui);
+
+    /**
+     * @brief Based on the received message type
+     * performs action on GUI
+     * 
+     * @param msg Message to perform action based on
+     */
     void performAction(Message &msg);
 };
